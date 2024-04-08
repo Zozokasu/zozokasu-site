0b88299dfa6552b98be241b6f0798288f2c64e4c
ぞぞかす <jojohounds@gmail.com>
2024-04-08 09:51:30 +0900

    めっちゃ内容ふやした

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>ぞぞかす</h1>');
+    res.sendFile(__dirname + '/index.html')
 });
+
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`[server]: Server is running on port ${port}`);
